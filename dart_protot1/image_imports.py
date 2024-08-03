b70d1cb79a59f1a5df26da1007d7aefc05e40634
Lea <s1953043@ed.ac.uk>
2024-08-03 15:10:02 +0200

    Add open file dialog

--- dart_protot1/image_imports.py
+++ dart_protot1/image_imports.py
@@ -1,16 +1,29 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, filedialog
+from settings import *
 
 class ImageImport(ttk.Frame):
     """
     (Frame + Button) to import image and/or folder.
     Opens the file explorer dialog for election
     """
-    def __init__(self, parent):
+    def __init__(self, parent, import_img_func):
+        # the frame
         super().__init__(master=parent)
         # TODO: cover entire RIGHT SIDE
         self.grid(row=0,  column=1,  padx=5,  pady=5,  sticky='nsew')
+        
+        self.import_img_func = import_img_func
 
-        # button, should say "select image (folder)"
-        import_imgs_button = ttk.Button(master=self, text="select image(s)")
+        # the button
+        import_imgs_button = ttk.Button(master=self, text="select image(s)", command=self.open_dialog)
         import_imgs_button.pack(expand=True)
+
+    def open_dialog(self):
+        path = filedialog.askopenfile().name
+        self.import_img_func(path)
+
+class ImageOutput(tk.Canvas):
+    def __init__(self, parent):
+        super().__init__(master=parent, background=BACKGROUND_COLOR, bd=0, relief="ridge")
+        self.grid(row=0, column=0, sticky="nsew")
--- dart_protot1/main.py
+++ dart_protot1/main.py
@@ -25,60 +25,68 @@ class App(tk.Tk):
 
         # layout
         self.rowconfigure(0, weight=1)
-        self.rowconfigure(1, weight=1)
-        self.rowconfigure(2, weight=1)
+        # self.rowconfigure(1, weight=1)
+        # self.rowconfigure(2, weight=1)
         self.columnconfigure(0, weight=6)
         self.columnconfigure(1, weight=2)
 
         # widgets
         # ImportButton (Frame + Button)
-        self.import_imgs_button = ImageImport(self)
+        self.import_imgs_button = ImageImport(self, self.import_image_func)
+        # message to select folder. TODO: update 
+        self.no_files_selected = ttk.Label(self,  text="No image(s) selected.")
+        self.no_files_selected.grid(row=0,  column=0,  padx=5,  pady=5)
 
         # Create left and right frames
-        left_frame  =  ttk.Frame(self, width=650,  height=400)
-        left_frame.grid(row=0,  column=0, rowspan=3,  padx=10,  pady=10)
-        right_frame  =  ttk.Frame(self, width=650,  height=400)
-        right_frame.grid(row=0,  column=1, rowspan=3, padx=10,  pady=10)
+        # left_frame  =  ttk.Frame(self, width=650,  height=400)
+        # left_frame.grid(row=0,  column=0, rowspan=3,  padx=10,  pady=10)
+        # right_frame  =  ttk.Frame(self, width=650,  height=400)
+        # right_frame.grid(row=0,  column=1, rowspan=3, padx=10,  pady=10)
 
         # right_frame_top  =  ttk.Frame(right_frame, width=200,  height=200)
         # right_frame_top.grid(row=0,  column=1,  padx=10,  pady=25)
-        right_frame_middle  =  ttk.Frame(right_frame, width=200,  height=400)
-        right_frame_middle.grid(row=1,  column=1,  padx=10,  pady=25)
-        right_frame_bottom  =  ttk.Frame(right_frame, width=200,  height=200)
-        right_frame_bottom.grid(row=2,  column=1,  padx=10,  pady=25)
-
-        # Create frames and labels
-        ttk.Label(left_frame,  text="Image (/folder) selected: ").grid(row=0,  column=0,  padx=5,  pady=5)
+        # right_frame_middle  =  ttk.Frame(right_frame, width=200,  height=400)
+        # right_frame_middle.grid(row=1,  column=1,  padx=10,  pady=25)
+        # right_frame_bottom  =  ttk.Frame(right_frame, width=200,  height=200)
+        # right_frame_bottom.grid(row=2,  column=1,  padx=10,  pady=25)
         
-        self.image = Image.open(resource_path("01_training.tif"))  # TODO: relpath saving? 2
+        # self.image = Image.open(resource_path("01_training.tif"))  # TODO: relpath saving? 2
         # resized_image = self.image.resize((650, 400))
-        self.image_tk = ImageTk.PhotoImage(self.image)
+        # self.image_tk = ImageTk.PhotoImage(self.image)
 
-        ttk.Label(left_frame,  image=self.image_tk).grid(row=1,  column=0,  padx=5,  pady=5)
+        # ttk.Label(left_frame,  image=self.image_tk).grid(row=1,  column=0,  padx=5,  pady=5)
         # Label(right_frame,  image=self.image_tk,  bg='grey').grid(row=0,  column=0,  padx=5,  pady=5)
 
-        def clicked():
-            '''if button is clicked, display message'''
-            print("Clicked.")
+        # def clicked():
+        #     '''if button is clicked, display message'''
+        #     print("Clicked.")
 
         # For now, when the buttons are clicked, they only call the clicked() method. We will add functionality later.
         # ttk.Button(right_frame_top,  text="Select image (/ folder)",  command=clicked).grid(row=0,  column=0,  padx=5,  pady=5,  sticky='w'+'e'+'n'+'s')
 
         # Example labels that serve as placeholders for other widgets
-        ttk.Label(right_frame_middle,  text="Crop edges",  relief="raised").grid(row=0,  column=0,  padx=5,  pady=3,  ipadx=10)
-        ttk.Label(right_frame_middle,  text="other toggle",  relief="raised").grid(row=0,  column=1,  padx=5,  pady=3,  ipadx=10)
-        ttk.Label(right_frame_middle,  text="export options",  relief="raised").grid(row=1,  column=1,  padx=5,  pady=3,  ipadx=10)
+        # ttk.Label(right_frame_middle,  text="Crop edges",  relief="raised").grid(row=0,  column=0,  padx=5,  pady=3,  ipadx=10)
+        # ttk.Label(right_frame_middle,  text="other toggle",  relief="raised").grid(row=0,  column=1,  padx=5,  pady=3,  ipadx=10)
+        # ttk.Label(right_frame_middle,  text="export options",  relief="raised").grid(row=1,  column=1,  padx=5,  pady=3,  ipadx=10)
 
-        # tk.Button(tool_bar,  text="Select csv output location",  command=clicked).grid(row=2,  column=0,  padx=5,  pady=5,  sticky='w'+'e'+'n'+'s')
-        # tk.Button(tool_bar,  text="(select output file type)",  command=clicked).grid(row=2,  column=1,  padx=5,  pady=5,  sticky='w'+'e'+'n'+'s')
+        # # tk.Button(tool_bar,  text="Select csv output location",  command=clicked).grid(row=2,  column=0,  padx=5,  pady=5,  sticky='w'+'e'+'n'+'s')
+        # # tk.Button(tool_bar,  text="(select output file type)",  command=clicked).grid(row=2,  column=1,  padx=5,  pady=5,  sticky='w'+'e'+'n'+'s')
        
-        # tk.Button(tool_bar,  text="Black &amp; White",  command=clicked).grid(row=1,  column=1,  padx=5,  pady=5,  sticky='w'+'e'+'n'+'s')
+        # # tk.Button(tool_bar,  text="Black &amp; White",  command=clicked).grid(row=1,  column=1,  padx=5,  pady=5,  sticky='w'+'e'+'n'+'s')
 
-        ttk.Button(right_frame_bottom,  text="Calculate DART value",  command=clicked).grid(row=0,  column=0,  padx=5,  pady=5,  sticky='w'+'e'+'n'+'s')
-        ttk.Label(right_frame_bottom, text="DART VALUE: ", font="30").grid(row=1,  column=0,  padx=5,  pady=5)
+        # ttk.Button(right_frame_bottom,  text="Calculate DART value",  command=clicked).grid(row=0,  column=0,  padx=5,  pady=5,  sticky='w'+'e'+'n'+'s')
+        # ttk.Label(right_frame_bottom, text="DART VALUE: ", font="30").grid(row=1,  column=0,  padx=5,  pady=5)
 
         # run
         self.mainloop()
+
+    def import_image_func(self, path):
+        print("path:", path)  # DEBUG
+        self.import_imgs_button.grid_forget()
+        self.no_files_selected.grid_forget()
+
+        self.image_output = ImageOutput(self)
+
 
 App()
 
